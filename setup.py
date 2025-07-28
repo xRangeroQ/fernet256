de51a97409f080bf5336307dc34da401bb991766
xRangeroQ <82023366+xRangeroQ@users.noreply.github.com>
2025-07-28 18:07:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fernet256",
-    version="0.1.0",
+    version="3.0.0",
     author="xRangeroQ",
     description="Simplified AES-256 encryption tool inspired by Fernet",
     long_description=open("README.md", encoding="utf-8").read(),
@@ -10,9 +10,7 @@ setup(
     url="https://github.com/xRangeroQ/fernet256",
     packages=find_packages(),
     install_requires=[
-        "cryptography>=41.0.0",
-        "numpy>=1.24.0",
-        "pytest>=7.0.0"
+        "pycryptodome>=3.23.0"
     ],
     python_requires=">=3.9",
 )
